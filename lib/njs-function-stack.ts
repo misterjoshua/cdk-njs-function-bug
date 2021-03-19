87d21a1cf08f147d3f3647f94e97697beb624f92
Josh Kellendonk <joshkellendonk@gmail.com>
2021-03-18 19:09:37 -0600

    use the node 14 runtime

--- lib/njs-function-stack.ts
+++ lib/njs-function-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as lambda_nodejs from '@aws-cdk/aws-lambda-nodejs';
 
 export class NjsFunctionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda_nodejs.NodejsFunction(this, 'function');
+    new lambda_nodejs.NodejsFunction(this, 'function', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+    });
   }
 }
