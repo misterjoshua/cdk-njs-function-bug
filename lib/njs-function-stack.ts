9007874d3fbfb4bc85411c4b3c1cab27691ea897
Josh Kellendonk <joshkellendonk@gmail.com>
2021-03-18 19:02:13 -0600

    add the buggy behaviour

---
+++ lib/njs-function-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda_nodejs from '@aws-cdk/aws-lambda-nodejs';
+
+export class NjsFunctionStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new lambda_nodejs.NodejsFunction(this, 'function');
+  }
+}
